f1bf3fa0457d8d303ffad319bc225ed4ad6eaada
operation1 <operation1@hanoisuntravel.com>
2026-07-19 16:53:32 +0700

    fix

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,10 +1,10 @@
-import { readFileSync } from 'fs'
-import { join } from 'path'
+import { readFileSync } from "fs";
+import { join } from "path";
 
-export const contentType = 'image/png'
-export const size = { width: 16, height: 16 }
+export const contentType = "image/png";
+export const size = { width: 16, height: 16 };
 
 export default function Icon() {
-  const buf = readFileSync(join(process.cwd(), 'public', 'logo-hns-16x16.png'))
-  return new Response(buf, { headers: { 'Content-Type': 'image/png' } })
+  const buf = readFileSync(join(process.cwd(), "public", "logo-hns-sun.png"));
+  return new Response(buf, { headers: { "Content-Type": "image/png" } });
 }
